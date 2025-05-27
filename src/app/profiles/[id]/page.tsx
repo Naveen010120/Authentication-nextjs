6fc36a6c6253fe967cdcfb8acfdb9de77ac98db3
NaveenSingh <raghuvamshinaveensingh@gmail.com>
2025-05-27 11:13:30 +0530

    remove async function in dynamic rooting and fixed the prop error

--- src/app/profiles/[id]/page.tsx
+++ src/app/profiles/[id]/page.tsx
@@ -5,7 +5,7 @@ interface Props{
 interface PropId{
     id:string
 }
-export default async function UserProfilePage({ params }: Props) {
+export default  function UserProfilePage({ params }: Props) {
     const {id}=params;
   return <div className="bg-gray-900 text-xl text-white font-bold flex justify-center h-screen items-center ">UserPage: <span className="p-2 bg-orange-500 rounded-lg font-bold text-black">{id}</span></div>;
 }
